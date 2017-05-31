c9f0688c5e7651c93116b9725567e1900e7d35ba
liyikun <liyikun@le.com>
2017-05-31 19:20:49 +0800

    upload solution of 496 Next Greater Element I

---
+++ src/com/leetcode/L496NextGreaterElementI.java
@@ -0,0 +1,85 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author liyikun 2017/05/31
+ * @see
+ *      <a href=
+ *      "https://leetcode.com/problems/next-greater-element-i/#/description">
+ *      496. Next Greater Element I</a>
+ * @description
+ *              You are given two arrays (without duplicates) nums1 and nums2
+ *              where nums1’s elements are subset of nums2. Find all the next
+ *              greater numbers for nums1's elements in the corresponding places
+ *              of nums2. The Next Greater Number of a number x in nums1 is the
+ *              first greater number to its right in nums2. If it does not
+ *              exist, output -1 for this number.
+ *              Example 1:
+ *              Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ *              Output: [-1,3,-1]
+ *              Explanation:
+ *              For number 4 in the first array, you cannot find the next
+ *              greater number for it in the second array, so output -1.
+ *              For number 1 in the first array, the next greater number for it
+ *              in the second array is 3.
+ *              For number 2 in the first array, there is no next greater number
+ *              for it in the second array, so output -1.
+ *              Example 2:
+ *              Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ *              Output: [3,-1]
+ *              Explanation:
+ *              For number 2 in the first array, the next greater number for it
+ *              in the second array is 3.
+ *              For number 4 in the first array, there is no next greater number
+ *              for it in the second array, so output -1.
+ *              Note:
+ *              All elements in nums1 and nums2 are unique.
+ *              The length of both nums1 and nums2 would not exceed 1000.
+ */
+public class L496NextGreaterElementI {
+
+    public static void main(String[] args) {
+        Solution solution = new Exhaustion();
+        System.out.println(Arrays.toString(solution.nextGreaterElement(
+                new int[] { 4, 1, 2 }, new int[] { 1, 3, 4, 2 })));
+        System.out.println(Arrays.toString(solution.nextGreaterElement(
+                new int[] { 2, 4 }, new int[] { 1, 2, 3, 4 })));
+    }
+
+    public static interface Solution {
+
+        public int[] nextGreaterElement(int[] findNums, int[] nums);
+    }
+
+    public static class Exhaustion implements Solution {
+
+        @Override
+        public int[] nextGreaterElement(int[] findNums, int[] nums) {
+            if (findNums == null || nums == null) {
+                return null;
+            }
+            if (findNums.length == 0 || nums.length == 0) {
+                return new int[] {};
+            }
+            int[] nextGreaterElements = new int[findNums.length];
+            for (int i = 0; i < findNums.length; i++) {
+                boolean isFindedNumber = false;
+                nextGreaterElements[i] = -1;
+                for (int j = 0; j < nums.length; j++) {
+                    if (isFindedNumber) {
+                        if (nums[j] > findNums[i]) {
+                            nextGreaterElements[i] = nums[j];
+                            break;
+                        }
+                    } else {
+                        isFindedNumber = findNums[i] == nums[j];
+                    }
+                }
+            }
+            return nextGreaterElements;
+        }
+
+    }
+
+}
