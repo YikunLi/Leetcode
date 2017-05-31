1059527e4d34f83d80f9e569a9bfc9f726395028
liyikun <liyikun@le.com>
2017-05-31 11:07:49 +0800

    update solution used dynamic programming of 396 Rotate Function

--- src/com/leetcode/L396RotateFunction.java
+++ src/com/leetcode/L396RotateFunction.java
@@ -21,7 +21,7 @@ public class L396RotateFunction {
 
     public static void main(String[] args) {
         int[] A = { 4, 3, 2, 6 };
-        Solution solution = new ExhaustionReduceSpace();
+        Solution solution = new DynamicProgramming();
         System.out.println(solution.maxRotateFunction(A));
     }
 
@@ -45,7 +45,8 @@ public class L396RotateFunction {
             for (int i = 0; i < arrays.length; i++) {
                 arrays[i] = new int[A.length];
                 for (int j = 0; j < arrays[i].length; j++) {
-                    arrays[i][j] = A[(i + j) % A.length];
+                    arrays[i][j] = A[((A.length - i) % A.length + j)
+                            % A.length];
                 }
                 // System.out.println(Arrays.toString(arrays[i]));
             }
@@ -77,9 +78,51 @@ public class L396RotateFunction {
             for (int i = 0; i < A.length; i++) {
                 int fk = 0;
                 for (int j = 1; j < A.length; j++) {
-                    int value = A[(i + j) % A.length];
+                    int value = A[((A.length - i) % A.length + j) % A.length];
                     fk += value * j;
                 }
+                maxValue = maxValue > fk ? maxValue : fk;
+            }
+            return maxValue;
+        }
+
+    }
+
+    /**
+     * Derivation of equation:
+     * F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n - 1) * Bk[n-1].
+     * F(k+1) = 0 * Bk[n-1] + 1 * Bk[0] + 2 * Bk[1] + ... + (n-1) * Bk[n-2]
+     * F(k+1) - F(k) = Bk[0] + Bk[1] + ... + Bk[n-2] + (1 - n) * Bk[n-1].
+     * F(k+1) - F(k) = Sum - n * Bk[n-1].
+     * F(k+1) = F(k) + Sum - n * Bk[n-1].
+     * Example:
+     * A = [4, 3, 2, 6]
+     * F(0) = F(3) + Sum - 4 * 4;
+     * Sum = 4 + 3 + 2 + 6 = 15;
+     * F(0) = 25
+     * F(1) = 26
+     * 25 = 26 + 15 - 16;
+     */
+    public static class DynamicProgramming implements Solution {
+
+        @Override
+        public int maxRotateFunction(int[] A) {
+            if (A == null || A.length == 0 || A.length == 1) {
+                return 0;
+            }
+            // Compute sum and F(0)
+            int sum = 0;
+            int f0 = 0;
+            for (int i = 0; i < A.length; i++) {
+                sum += A[i];
+                f0 += i * A[i];
+            }
+            // Compare
+            int maxValue = f0;
+            int fk = f0;
+            for (int i = 1; i < A.length; i++) {
+                fk += sum - A.length * A[A.length - i];
+                // System.out.println(fk);
                 maxValue = maxValue > fk ? maxValue : fk;
             }
             return maxValue;
